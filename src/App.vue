3461dcb62755cecd507cfc703bd1644d0657bde3
Emery Porter <emeryporter99@gmail.com>
2019-11-07 21:10:54 -0600

    Fixed bug where I disabled all functionality
    oops

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,28 @@
       <h1 class="display-1">Local Info</h1>
     </b-row>
     <b-row class="p-3">
-      <LocInput :disabled="numReady === 0" @submit="onSubmit"/>
+      <LocInput :disabled="disableInput" @submit="onSubmit" />
     </b-row>
     <b-row v-if="location !== ''">
       <b-col>
-        <Map v-show="numReady == 2" :location="location" @ready="onReady"/>
+        <Map v-show="numReady == 2" :location="location" @ready="onReady" />
       </b-col>
       <b-spinner v-if="numReady < 2" style="width: 3rem; height: 3rem;"></b-spinner>
       <b-col>
         <Weather v-show="numReady == 2" :location="location" @ready="onReady" />
       </b-col>
-     
     </b-row>
     <b-row align-h="center" class="fixed-bottom">
-      <p class="text-muted text-center font-weight-light">
-        Built by Dalton Eschberger, Emery Porter, Jeffrey Lance, and Mark Stelluti.
-      </p>
+      <p
+        class="text-muted text-center font-weight-light"
+      >Built by Dalton Eschberger, Emery Porter, Jeffrey Lance, and Mark Stelluti.</p>
     </b-row>
   </b-container>
 </template>
 
 <script>
-const Map = () => import('./components/Map');
-const Weather = () => import('./components/Weather');
+const Map = () => import("./components/Map");
+const Weather = () => import("./components/Weather");
 import LocInput from "./components/Input";
 import { BContainer, BRow, BCol, BSpinner } from "bootstrap-vue";
 export default {
@@ -43,18 +42,25 @@ export default {
   data() {
     return {
       location: "",
-      numReady: 0
+      numReady: 0,
+      disableInput: false
     };
   },
   methods: {
     onSubmit(loc) {
-      if(this.location !== loc){
-        this.$set(this,"numReady", 0);
+      if (this.location !== loc) {
+        this.$set(this, "numReady", 0);
+        this.$set(this, "disableInput", true);
       }
       this.$set(this, "location", loc);
     },
-    onReady(){
-      this.$set(this,"numReady", this.numReady + 1);
+    onReady() {
+      this.$set(this, "numReady", this.numReady + 1);
+      this.$nextTick(() => {
+        if (this.numReady === 2) {
+          this.$set(this, "disableInput", false);
+        }
+      });
     }
   }
 };
